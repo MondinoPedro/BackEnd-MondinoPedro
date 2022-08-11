a4c9694460c82c98e0d311494917d509c397455e
MondinoPedro <mondinopedro12@gmail.com>
2022-08-11 13:57:46 -0300

    Quinto Desafio

---
+++ Desafio5/Ejs/server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+
+const PORT= 4000 || process.env.PORT
+
+app.use(express.json())
+app.use(express.urlencoded({ extended : true }))
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+let productos = []
+
+app.get('/', (req,res)=>{
+    res.render('pages/index', {productoAgregado: null})
+    
+})
+
+app.post('/', (req,res)=>{
+    const {nombre, precio, descripcion} = req.body
+    productos.push({'nombre': nombre, 'precio': precio, 'descripcion': descripcion})
+    res.redirect('/productos')
+})
+
+app.get('/productos', (req,res)=> {
+    res.render('pages/index', {productos:productos, productoAgregado: true})
+})
+
+app.post('/productos', (req,res)=>{
+    res.redirect('/')
+})
+
+app.listen(PORT, err =>{
+    if (err) throw err
+    console.log(`Escuchando el puerto ${PORT}`)
+})
